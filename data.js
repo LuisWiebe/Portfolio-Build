96a5dd9a61f7ef8986f038d7da0bd3334d823025
Chelsea Vick <wiebe.shayne@gmail.com>
2026-03-23 10:27:01 -0700

    dddddd

---
+++ data.js
@@ -0,0 +1,85 @@
+const defaultProject = {
+    id: "",
+    slug: "/my-sick-as-hell-project",
+    title: "My sick as hell project",
+    description: "This project was worked on during class of a space princess.",
+    category: "drawings",
+    createAt: "2025-10-13T12:15:15:06Z",
+    photo: {
+        url: "https://designfordummies.com/mypath/to/this/photo.jpg",
+        alt: "This is a photo of the project in progress",
+        title: "Progression of my project",
+    },
+    video: {
+        url: "https://designfordummies.com/mypath/to/this/video.mp4",
+        provider: "youtube",
+    },
+    process: [
+        {
+            title: "This is my sketch",
+            description: "I started in my sketchbook and drew out my ideas.",
+            photo: {
+                url: "https://designfordummies.com/mypath/to/this/photo.jpg",
+                alt: "This is a photo of the project in progress",
+                title: "Progression of my project",
+            },
+            type: "Mood board",
+        },
+    ]
+
+
+}
+
+const portfolio = [
+    {
+        id: "",
+        slug: "/celeb-red-teas-logo",
+        title: "Celeb Red Tea Logo",
+        description: "These projects were created within the first year of my graphic design classes.",
+        category: "Vector Illustrations",
+        createAt: "2025-11-17T10:22:36:06Z",
+        photo: {
+            url: "Logo.png",
+            alt: "This logo is for a fictional tea brand, the character is Astarion from Baldurs Gate 3.",
+            title: "Vampyre Tea Logo",
+        },
+        video: {},
+        process: []
+
+
+    },
+    {
+        id: "",
+        slug: "/illustrative-set",
+        title: "Illustrative Set",
+        description: "This project was an exercise of making icons within a set color scheme.",
+        category: "Vector Illustrations",
+        createAt: "2025-09-22T10:17:57:06Z",
+        photo: {
+            url: "GDES131-IllustrativeSet-FA25-Landscape-LWiebe.png",
+            alt: "These are the illustrative icons I made in illustrator.",
+            title: "Analougous Color Scheme Icon Set",
+        },
+        video: {},
+        process: []
+
+
+    },
+    {
+        id: "",
+        slug: "/art-nouveau-poster",
+        title: "Art Nouveau Poster",
+        description: "An Art Nouveau inspired poster of a woman posed against a bike.",
+        category: "Vector Illustrations",
+        createAt: "2025-10-27T02:20:03:06Z",
+        photo: {
+            url: "poster.png",
+            alt: "Art Nouveau poster",
+            title: "Art Nouveau Poster.",
+        },
+        video: {},
+        process: []
+
+
+    }
+]
